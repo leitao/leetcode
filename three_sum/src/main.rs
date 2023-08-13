f292aaeba5530526f844f5a297f009a753b4e528
Breno Leitao <leitao@debian.org>
2023-08-13 09:17:20 +0100

    Solved

--- three_sum/src/main.rs
+++ three_sum/src/main.rs
@@ -1,30 +1,40 @@
 struct Solution;
 
 impl Solution {
-    pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>>
+    pub fn three_sum(numm: Vec<i32>) -> Vec<Vec<i32>>
     {
         let mut ret: Vec<Vec<i32>> = Vec::new();
+        let mut nums = numm.clone();
+        nums.sort();
 
         for i in 0..nums.len() {
-            for j in 0..nums.len() {
-                for k in 0..nums.len() {
-                    if (j != k && k != i && i != j) && (nums[i] + nums[j] + nums[k] == 0) {
-                        let mut to_add = vec![nums[i], nums[j], nums[k]];
-                        to_add.sort();
-                        if !ret.contains(&to_add) {
-                            ret.push(to_add);
+            for j in i..nums.len() {
+                if i == j {
+                    continue;
+                }
+                let missing =  - (nums[i] + nums[j]);
 
-                        }
+                let contains = nums[j+1..].binary_search(&missing);
+                if contains.is_err() {
+                    continue;
+                }
 
-                    }
+                let mut to_add = vec![nums[i], nums[j], missing];
+                to_add.sort();
+
+                if !ret.contains(&to_add) {
+                    ret.push(to_add);
                 }
             }
         }
     return ret;
     }
 }
+
 fn main() {
+    // let n: Vec<i32> = vec![-1,0,1,2,-1,-4];
     let n: Vec<i32> = vec![-1,0,1,2,-1,-4];
+    println!("Array is {:?}", n);
     let ret = Solution::three_sum(n);
     println!("= {:?}",  ret);
 }
