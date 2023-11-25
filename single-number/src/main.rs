2c53f6d3cff582cc9241afa6f32beb719a8c157e
Breno Leitao <leitao@debian.org>
2023-11-25 10:33:16 +0000

    second try. Not fast enough

---
+++ single-number/src/main.rs
@@ -0,0 +1,36 @@
+use std::collections::HashSet;
+
+struct Solution;
+
+impl Solution {
+    pub fn single_number(nums: Vec<i32>) -> i32 {
+        let mut h: HashSet<i32> = HashSet::new();
+
+        for i in 0..nums.len() {
+            let val = nums[i];
+            if h.contains(&val) {
+                h.remove(&val);
+                continue;
+            }
+
+            h.insert(val);
+        }
+
+        if h.len() != 1 {
+            panic!("{:?} hs more than one element", h);
+        }
+        for element in h {
+            return element;
+        }
+
+        panic!("error\n");
+    }
+}
+
+fn main() {
+    // let v = vec![1,2,3,1,3];
+    let v = vec![4,1,2,1,2];
+    println!("{:?}", v);
+    let a = Solution::single_number(v);
+    println!("{}", a); 
+}
